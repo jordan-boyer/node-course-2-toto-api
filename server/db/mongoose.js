3f3dd4b8f7d4f860188d543030c7112ec9794c49
Jordan Boyer <jordan.boyer@epitech.eu>
2018-05-03 11:51:54 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 
+var link = '';
+
+if (process.env.PORT) {
+    link = 'mongodb://jordan:finalfantasy@ds247479.mlab.com:47479/todo-api'
+} else {
+    link = 'mongodb://localhost:27017/TodoApp'
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(link);
 
 module.exports = {mongoose};
