b858ee013f71af91a10facb895768b2d268586e8
Jordan Boyer <jordan.boyer@epitech.eu>
2018-05-07 15:03:17 +0200

    Hash password

--- playground/haising.js
+++ playground/haising.js
@@ -1,7 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+
+var password = '123abc!'
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$iWzDaVovgxpIcKI7Q3t5/eyekRzt9tFaJPXQE0N1VdsTXqw4gSPhm'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+
+/*var data = {
     id: 10
 };
 
@@ -9,7 +25,7 @@ var token = jwt.sign(data, '123abc');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+console.log('decoded', decoded);*/
 /* var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
